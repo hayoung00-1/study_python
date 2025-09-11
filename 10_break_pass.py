bfd5ecf5df831fce9793195561c37a87757c2f3a
백하영 <Back ha young@email.com>
2025-09-11 11:19:46 +0900

    continue 문 추가

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -1,12 +1,18 @@
-cup = 0
-running = True
-
-while True:
-    cup += 1
-    print(cup)
-    if cup == 10:
-        #running = False
-        break
+#cup = 0
+#running = True
 
 
-print('while 문 종료')
+#print('while 문 종료')
+
+for i in range(1,10):
+    if i == 3:
+        continue
+
+    if i == 6:
+        continue
+
+    if i == 9:
+        continue
+
+    print(i)
+

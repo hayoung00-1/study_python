3c75b2d5b8221ea093283e8d4b46503e6b015bea
백하영 <Back ha young@email.com>
2025-09-12 15:43:22 +0900

    함수리스트2

---
+++ 20_function_list.py
@@ -0,0 +1,27 @@
+# 1.반환타입: O매개변수:O 인형뽑기,커피그라인더
+# 2.반환타입: O매개변수:X번호표기기
+# 3.반환타입: X매개변수:O오락실게임
+# 4.반환타입: X매개변수:X호출벨
+
+def hole(dull):
+    print (f'{dull}을 뽑았습니다.')
+    return f'인형이 나왔습니다.'
+
+co=hole('토끼인형')
+print(co)
+
+def 번호표기계():
+    return "번호표"
+print(번호표기계())
+
+
+def bell():
+    print('호출!!!')
+button = bell()
+print(button)
+
+
+
+
+
+

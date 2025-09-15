cce2224a1ec817cd1a2cda0351961ec5b9abef18
백하영 <Back ha young@email.com>
2025-09-15 11:17:59 +0900

    모듈 생성

---
+++ advance.py
@@ -0,0 +1,18 @@
+#변수
+field1 =10
+field2 = 'String'
+
+#함수
+def sum(a,b):
+    return a+b
+
+def sum(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def devision(a,b):
+    return a/b
+
+
